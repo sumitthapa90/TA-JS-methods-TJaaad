dfddaa96be5787ca308d14d33b3ae77868848a82
sumit <sumitthapa90@gmail.com>
2021-02-18 20:23:50 +0530

    complete

--- block-BJaadr/code/index.js
+++ block-BJaadr/code/index.js
@@ -1,35 +1,67 @@
 // NOTE: You can only use the (reduce) array method to solve this exercise:
 
 function countAllPeople() {
-  // your code goes here
+  return got.houses.reduce((acc, cv) => {
+    return acc + cv.people.length;
+  }, 0);
 }
 
 function peopleByHouses() {
-  // your code goes here
+  return got.houses.reduce((acc, cv) => {
+    acc[cv.name] = cv.people.length;
+    return acc;
+  }, {});
 }
 
 function everyone() {
-  // your code goes here
+  return got.houses.reduce((acc, cv) => {
+    acc = acc.concat(cv.people.map((x) => x.name));
+    return acc;
+  }, []);
 }
 
 function nameWithS() {
-  // your code goes here
+  return got.houses.reduce((acc, cv) => {
+    acc = acc.concat(
+      cv.people.filter((x) => x.name.toLowerCase().includes("s"))
+    );
+    return acc;
+  }, []);
 }
 
 function nameWithA() {
-  // your code goes here
+  return got.houses.reduce((acc, cv) => {
+    acc = acc.concat(
+      cv.people.filter((x) => x.name.toLowerCase().includes("a"))
+    );
+    return acc;
+  }, []);
 }
 
 function surnameWithS() {
-  // your code goes here
+  return got.houses.reduce((acc, cv) => {
+    acc = acc.concat(
+      cv.people.filter((x) => x.name.split(" ")[1].toLowerCase().includes("s"))
+    );
+    return acc;
+  }, []);
 }
 
-function surnameWithA() {
-  // your code goes here
+function nameWithA() {
+  return got.houses.reduce((acc, cv) => {
+    acc = acc.concat(
+      cv.people.filter((x) => x.name.spilt(" ")[1].toLowerCase().includes("a"))
+    );
+    return acc;
+  }, []);
 }
+
 
 function peopleNameOfAllHouses() {
-  // your code goes here
+  return got.houses.reduce((acc, cv) => {
+    acc[cv.name] = cv.people.map((v) => v.name);
+    return acc;
+  }, {});
 }
 
 // Testing your result after writing your function
@@ -44,7 +76,7 @@ console.log(everyone());
 // Output should be
 //["Eddard "Ned" Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon "Bran" Stark", "Rickon Stark", "Jon Snow", "Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Queen Cersei (Lannister) Baratheon", "King Robert Baratheon", "Stannis Baratheon", "Renly Baratheon", "Joffrey Baratheon", "Tommen Baratheon", "Myrcella Baratheon", "Daenerys Targaryen", "Viserys Targaryen", "Balon Greyjoy", "Theon Greyjoy", "Yara Greyjoy", "Margaery (Tyrell) Baratheon", "Loras Tyrell", "Catelyn (Tully) Stark", "Lysa (Tully) Arryn", "Edmure Tully", "Brynden Tully", "Olenna (Redwyne) Tyrell", "Walder Frey", "Jon Arryn", "Khal Drogo"]
 
-console.log(nameWithS(), 'with s');
+console.log(nameWithS(), "with s");
 // Output should be
 // ["Eddard "Ned" Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon "Bran" Stark", "Rickon Stark", "Jon Snow", "Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Queen Cersei (Lannister) Baratheon", "Stannis Baratheon", "Daenerys Targaryen", "Viserys Targaryen", "Loras Tyrell", "Catelyn (Tully) Stark", "Lysa (Tully) Arryn"]
 
@@ -52,7 +84,7 @@ console.log(nameWithA());
 // Output should be
 // ["Eddard Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon Stark", "Rickon Stark", "Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Cersei Baratheon", "Robert Baratheon", "Stannis Baratheon", "Renly Baratheon", "Joffrey Baratheon", "Tommen Baratheon", "Myrcella Baratheon", "Daenerys Targaryen", "Viserys Targaryen", "Balon Greyjoy", "Yara Greyjoy", "Margaery Baratheon", "Loras Tyrell", "Catelyn Stark", "Lysa Arryn", "Olenna Tyrell", "Walder Frey", "Jon Arryn", "Khal Drogo"]
 
-console.log(surnameWithS(), 'surname with s');
+console.log(surnameWithS(), "surname with s");
 // Output should be
 // ["Eddard Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon Stark", "Rickon Stark", "Jon Snow", "Catelyn Stark"]
 
@@ -63,3 +95,4 @@ console.log(surnameWithA());
 console.log(peopleNameOfAllHouses());
 // Output should be
 // {Arryns: ["Jon Arryn"], Baratheons: ["Robert Baratheon", "Stannis Baratheon", "Renly Baratheon", "Joffrey Baratheon", "Tommen Baratheon", "Myrcella Baratheon"], Dothrakis: ["Khal Drogo"], Freys: ["Walder Frey"], Greyjoys: ["Balon Greyjoy", "Theon Greyjoy", "Yara Greyjoy"], Lannisters: ["Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Cersei Baratheon"], Redwyne: ["Olenna Tyrell"], Starks: ["Eddard Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon Stark", "Rickon Stark", "Jon Snow"], Targaryens: ["Daenerys Targaryen", "Viserys Targaryen"], Tullys: ["Catelyn Stark", "Lysa Arryn", "Edmure Tully", "Brynden Tully"], Tyrells: ["Margaery Baratheon", "Loras Tyrell"]}
+
